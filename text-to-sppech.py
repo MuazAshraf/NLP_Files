d81be70c990e7306be18b58f15022dd068f99cc2
Muaz <98591478+MuazAshraf@users.noreply.github.com>
2025-02-05 21:24:50 +0500

    Update text-to-sppech.py

--- text-to-sppech.py
+++ text-to-sppech.py
@@ -1,6 +1,12 @@
 from pathlib import Path
 from openai import OpenAI
-client = OpenAI(api_key = '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')
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=OPENAI_API_KEY)
+
 
 speech_file_path = Path(__file__).parent / "speech2.mp3"
 response = client.audio.speech.create(
